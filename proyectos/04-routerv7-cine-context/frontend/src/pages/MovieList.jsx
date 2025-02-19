b9a15e697d2c86ed758b389499be397e7a23848d
paco <pacopedrosa2018@gmail.com>
2025-02-19 08:45:31 +0100

    proyecto finalizado

--- proyectos/04-routerv7-cine-context/frontend/src/pages/MovieList.jsx
+++ proyectos/04-routerv7-cine-context/frontend/src/pages/MovieList.jsx
@@ -9,7 +9,10 @@ const MovieList = () => {
   const [sortBy, setSortBy] = useState('popularity.desc')
   const [minRating, setMinRating] = useState(0)
   
-  const { data, loading, error } = useFetch(() => getPopularMovies(page), [page])
+  const { data, loading, error } = useFetch(
+    () => getPopularMovies(page, sortBy), 
+    [page, sortBy]
+  )
 
   const filteredMovies = data?.results?.filter(
     movie => movie.vote_average >= minRating
